cfe3a9d660278f5278be0e6aa6c8e4e5abc0269d
RaquelCollado <rcolladoherrera@gmail.com>
2023-09-28 17:55:16 +0200

    add structure

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//constanes 
+const input = document.querySelector('.js-input');
+const btn = document.querySelector('.js-btn');
+const filmSection = document.querySelector('.js-films');
+const favoritesSection = document.querySelector('.js-favorites');
+
+//funciones
+
+
+//eventos
