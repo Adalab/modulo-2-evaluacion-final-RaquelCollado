ede6fae703d087da68bb27fca9f8577b704d2943
RaquelCollado <rcolladoherrera@gmail.com>
2023-10-02 11:34:42 +0200

    renderFavorite

--- src/js/main.js
+++ src/js/main.js
@@ -1,76 +1,86 @@
-"use strict";
+'use strict';
 
 //constanes
-const inputElement = document.querySelector(".js-input");
-const btnElement = document.querySelector(".js-btn");
-const serieSection = document.querySelector(".js-ulseries");
-const favoriteSection = document.querySelector(".js-ulFavorites");
+const inputElement = document.querySelector('.js-input');
+const btnElement = document.querySelector('.js-btn');
+const serieSection = document.querySelector('.js-ulseries');
+const favoriteSection = document.querySelector('.js-ulFavorites');
 
 //estas son mis variables para guardar la info que me genera la API.
 let serieList = [];
 let serieFavorite = [];
-//Local Storage
-const serieLS = JSON.parse(localStorage.getItem("serie"));
-
-
+//Local Storage //serie (item)
+// eslint-disable-next-line no-unused-vars
+const serieLS = JSON.parse(localStorage.getItem('serieFavorite'));
 //función cuando hago click en buscar.
 function handleClick(event) {
   event.preventDefault();
-
-  //pagina vacia al cargar.
-  serieSection.innerHTML = "";
+  //pagina vacia al cargar y buscador.
+  serieSection.innerHTML = '';
   const inputValue = inputElement.value;
   fetch(`//api.tvmaze.com/search/shows?q=${inputValue}`)
     .then((response) => response.json())
     .then((dataApi) => {
-
       //guardo los datos en el array que he creado.
       serieList = dataApi;
-
       //bucle listado
       for (const serie of dataApi) {
-
         //me pone la imagen por defecto en las que no la tienen.
         const imgDefault = `//via.placeholder.com/210x295/ffffff/666666/?text=TV`;
         const imgSrc =
-        serie.show.image && serie.show.image.medium ? serie.show.image.medium : imgDefault;
-
-        //me pinta la lista de series en la seccion. 
+          serie.show.image && serie.show.image.medium
+            ? serie.show.image.medium
+            : imgDefault;
+        //me pinta la lista de series en la seccion.
         serieSection.innerHTML += `<li id=${serie.show.id} class="card js-card" ><img class="image" src=${imgSrc} alt=""><h2>${serie.show.name}</h2></li>`;
       }
-
       //ejecuto la función que lo pinta dentro de mi ul.
       addEventToSeries();
     });
 }
-//handleFavorites crea las series en favoritos a traves de su id (donde se escucha) parseInt , para que me lo busque por numero y no por string.
-function handleFavorites(event) {
-  event.preventDefault();
-  const idSerie = parseInt(event.currentTarget.id);
+function renderFavorite(){
+  favoriteSection.innerHTML= '';
+  for (const serie of serieFavorite) {
+    //me pone la imagen por defecto en las que no la tienen.
+    const imgDefault = `//via.placeholder.com/210x295/ffffff/666666/?text=TV`;
+    const imgSrc =
+      serie.show.image && serie.show.image.medium
+        ? serie.show.image.medium
+        : imgDefault;
 
-  //array de fav = array api busca la id serie y es estrictamente igual al id serie
-  serieFavorite = serieList.find((serie)=> serie.show.id === idSerie);
-  //añado o elimino la clase al clicar.
-  event.currentTarget.classList.add("cardFavorite");
-  event.currentTarget.classList.remove("card");
-
-
-  //imagen por defecto en caso de no tener.
-  const imgDefault = `//via.placeholder.com/210x295/ffffff/666666/?text=TV`;
-  const imgSrc = serieFavorite.show.image && serieFavorite.show.image.medium ? serieFavorite.show.image.medium : imgDefault;
-
-
-  //ponga idSerie o serieFavorite hace lo mismo
-  favoriteSection.innerHTML += `<li id=${serieFavorite} class="card js-card" ><img class="image" src=${imgSrc} alt=""><h2>${serieFavorite.show.name}</h2></li>`;
-  localStorage.setItem("serie",JSON.stringify(serieFavorite));
-  }
-
-// addEventToSeries  me permite añadir las series a mi ul y las favoritas elegidas (click) a mi lista de series favoritas.
-function addEventToSeries() {
-  const allSeries = document.querySelectorAll(".js-card");
-  for (const serie of allSeries) {
-  serie.addEventListener("click", handleFavorites);
+    //me pinta la lista de series en la seccion.
+    favoriteSection.innerHTML += `<li id=${serie.show.id} class="card js-card" ><img class="image" src=${imgSrc} alt=""><h2>${serie.show.name}</h2></li>`;
+    event.currentTarget.classList.add('cardFavorite');
   }
 }
-
-btnElement.addEventListener("click", handleClick);
+//handleFavorites crea las series en favoritos a traves de su id
+function handleFavorites(event) {
+  event.preventDefault();
+  //paso id string a numero para que no sea undefine
+  const idSerie = parseInt(event.currentTarget.id);
+  //me devuelve el primer elemento que coincide con lo comparado -1 / 1
+  const index = serieFavorite.findIndex((serie) => serie.show.id === idSerie);
+  //si me devuelve -1 busca la id le añade la clase a la serie y añade la serie a favoritos
+  if (index === -1) {
+    const serieClick = serieList.find((serie) => serie.show.id === idSerie);
+    serieFavorite.push(serieClick);
+  } else {
+    event.currentTarget.classList.remove('card');
+    serieFavorite.splice(index, 1);
+    const favoriteSerie = document.getElementById(idSerie);
+    if (favoriteSerie) {
+      event.currentTarget.classList.remove('card');
+      event.currentTarget.classList.add('cardFavorite');
+    }
+  }
+  renderFavorite();
+  localStorage.setItem('serieFavorite', JSON.stringify(serieFavorite));
+}
+// addEventToSeries  me permite añadir las series a mi ul y las favoritas elegidas (click) a mi lista de series favoritas.
+function addEventToSeries() {
+  const allSeries = document.querySelectorAll('.js-card');
+  for (const serie of allSeries) {
+    serie.addEventListener('click', handleFavorites);
+  }
+}
+btnElement.addEventListener('click', handleClick);
